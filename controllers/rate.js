eab7b655ded4c76f14392b56525dea1e3d380426
Néstor Iriondo <nestoririondo@gmail.com>
2024-01-29 08:45:51 +0100

    device_info added to db

--- controllers/rate.js
+++ controllers/rate.js
@@ -1,15 +1,22 @@
 import pool from "../db.js";
 
 export const postRate = async (req, res) => {
-  const { image_id, value } = req.body;
-  console.log("post request", image_id, value);
+  const { image_id, value, device_info } = req.body;
+  console.log("post request", image_id, value, device_info);
   try {
     const { rows } = await pool.query(
       `
-      INSERT INTO cat_ratings (CatImageID, Score, Date) 
-      VALUES ($1, $2, NOW())
-      `,
-      [image_id, value]
+        INSERT INTO cat_ratings (CatImageID, Score, Date, userAgent, language, screenWidth, screenHeight) 
+        VALUES ($1, $2, NOW(), $3, $4, $5, $6)
+        `,
+      [
+        image_id,
+        value,
+        device_info.userAgent,
+        device_info.language,
+        device_info.screenResolution.width,
+        device_info.screenResolution.height,
+      ]
     );
     res.status(201).json(rows);
   } catch (error) {
