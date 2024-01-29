f826b8cb072f713d7557f5abd9a6d27542ec7df0
Néstor Iriondo <nestoririondo@gmail.com>
2024-01-29 08:57:20 +0100

    rollback

--- controllers/rate.js
+++ controllers/rate.js
@@ -1,22 +1,15 @@
 import pool from "../db.js";
 
 export const postRate = async (req, res) => {
-  const { image_id, value, device_info } = req.body;
-  console.log("post request", image_id, value, device_info);
+  const { image_id, value } = req.body;
+  console.log("post request", image_id, value);
   try {
     const { rows } = await pool.query(
       `
-        INSERT INTO cat_ratings (CatImageID, Score, Date, userAgent, language, screenWidth, screenHeight) 
-        VALUES ($1, $2, NOW(), $3, $4, $5, $6)
-        `,
-      [
-        image_id,
-        value,
-        device_info.userAgent,
-        device_info.language,
-        device_info.screenResolution.width,
-        device_info.screenResolution.height,
-      ]
+      INSERT INTO cat_ratings (CatImageID, Score, Date) 
+      VALUES ($1, $2, NOW())
+      `,
+      [image_id, value]
     );
     res.status(201).json(rows);
   } catch (error) {
@@ -28,11 +21,11 @@ export const postRate = async (req, res) => {
 export const getRates = async (req, res) => {
   try {
     const { rows } = await pool.query(`
-    SELECT CatImageID, SUM(Score) as TotalScore
-    FROM cat_ratings
-    GROUP BY CatImageID
-    ORDER BY TotalScore DESC
-    LIMIT 10
+          SELECT CatImageID, SUM(Score) as TotalScore
+          FROM cat_ratings
+          GROUP BY CatImageID
+          ORDER BY TotalScore DESC
+          LIMIT 10
       `);
     res.status(200).json(rows);
     console.log("get request, scoreboard");
