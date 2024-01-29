98021a917c2f7e9f4e7a4a65baf64cbe0d8a65fa
Néstor Iriondo <nestoririondo@gmail.com>
2024-01-29 08:17:24 +0100

    voting db structure updated, no db modification

--- controllers/rate.js
+++ controllers/rate.js
@@ -4,31 +4,33 @@ export const postRate = async (req, res) => {
   const { image_id, value } = req.body;
   console.log("post request", image_id, value);
   try {
-      const { rows } = await pool.query(`
-          INSERT INTO cat_ratings (CatImageID, TotalScore, RatingCount, Date) 
-          VALUES ($1, $2, 1, NOW()) 
-          ON CONFLICT (CatImageID) 
-          DO UPDATE SET TotalScore = cat_ratings.TotalScore + $2, RatingCount = cat_ratings.RatingCount + 1, Date = NOW()
-      `, [image_id, value]);
-      res.status(201).json(rows);
+    const { rows } = await pool.query(
+      `
+      INSERT INTO cat_ratings (CatImageID, Score, Date) 
+      VALUES ($1, $2, NOW())
+      `,
+      [image_id, value]
+    );
+    res.status(201).json(rows);
   } catch (error) {
-      console.error(error);
-      res.status(400).json({ message: error.message });
+    console.error(error);
+    res.status(400).json({ message: error.message });
   }
 };
 
 export const getRates = async (req, res) => {
   try {
-      const { rows } = await pool.query(`
-          SELECT CatImageID, TotalScore, RatingCount
-          FROM cat_ratings
-          ORDER BY TotalScore DESC
-          LIMIT 10
+    const { rows } = await pool.query(`
+    SELECT CatImageID, SUM(Score) as TotalScore
+    FROM cat_ratings
+    GROUP BY CatImageID
+    ORDER BY TotalScore DESC
+    LIMIT 10
       `);
-      res.status(200).json(rows);
-      console.log("get request, scoreboard")
+    res.status(200).json(rows);
+    console.log("get request, scoreboard");
   } catch (error) {
-      console.error(error);
-      res.status(400).json({ message: error.message });
+    console.error(error);
+    res.status(400).json({ message: error.message });
   }
 };
